6bba40834441c840e9249d51c584a42e76602f60
ItsME-TM <thushanmalaka123@gmail.com>
2026-04-03 14:22:04 +0530

    feat: implement Roman to Integer conversion with test cases

---
+++ 13.roman-to-integer.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=13 lang=java
+ *
+ * [13] Roman to Integer
+ */
+
+// @lc code=start
+class Solution {
+    public int romanToInt(String s) {
+        Map <Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int total = 0;
+        for (int i = 0; i < s.length(); i++){
+            int current = map.get(s.charAt(i));
+            int next = (i + 1 < s.length()) ? map.get(s.charAt(i + 1)) : 0;
+            if(current >= next){
+                total += current;
+            }else{
+                total -= current;
+            }
+        }
+        return total;
+    }
+}
+// @lc code=end
+
