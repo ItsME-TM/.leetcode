949645678d190ff44af28856c935b635cc4eb362
ItsME-TM <thushanmalaka123@gmail.com>
2026-04-20 16:50:10 +0530

    feat: add solution for Add Binary with test case in main method

---
+++ 67.add-binary.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=67 lang=java
+ *
+ * [67] Add Binary
+ */
+
+// @lc code=start
+class Solution {
+    public String addBinary(String a, String b) {
+        if (a == null || a.length() == 0) {
+            return b;
+        }
+        if (b == null || b.length() == 0) {
+            return a;
+        }
+        StringBuilder sb = new StringBuilder();
+        int aPointer = a.length() - 1;
+        int bPointer = b.length() - 1;
+        int carryOver = 0;
+        int sum = 0;
+        while (aPointer >= 0 || bPointer >= 0 || carryOver > 0){
+            sum = carryOver;
+            if (aPointer >= 0){
+                sum = sum + a.charAt(aPointer) - '0';
+                aPointer--;
+            }
+            if (bPointer >= 0){
+                sum = sum + b.charAt(bPointer) - '0';
+                bPointer--;
+            }
+            sb.append(sum % 2);
+            carryOver = sum / 2;
+        }
+        return sb.reverse().toString();
+    }
+}
+// @lc code=end
+
