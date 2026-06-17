d58d908d5c98cd2f381d06a32af8a13d10c2cfeb
ItsME-TM <thushanmalaka123@gmail.com>
2026-06-17 07:35:17 +0530

    feat: add SQL implementation for Customers Who Never Order

---
+++ 183.customers-who-never-order.sql
@@ -0,0 +1,14 @@
+--
+-- @lc app=leetcode id=183 lang=mysql
+--
+-- [183] Customers Who Never Order
+--
+
+-- @lc code=start
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o 
+ON c.id = o.customerId
+WHERE o.id IS NULL
+-- @lc code=end
+
